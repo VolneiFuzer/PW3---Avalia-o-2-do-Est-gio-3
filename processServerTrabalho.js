81c505bad74058dadf7b0255b6d246b8d26e4993
Volnei Fuzer <volnei.fuzer@gmail.com>
2016-11-25 12:29:58 -0300

    finalizado

--- processServerTrabalho.js
+++ processServerTrabalho.js
@@ -10,16 +10,17 @@ app.get("/index.html", function(req, res){
 });
 
 app.post("/cadastrarTrabalho", urlEncodedParser, function(req, res){
+        
     response = {
-        inputEmpresa: req.body.inputEmpresa,
-        inputLogradouro: req.body.inputLogradouro,
+        inputEmpresa: validaEmpresa(req.body.inputEmpresa),
+        inputLogradouro: validaLogradouro(req.body.inputLogradouro),
         inputNumero: req.body.inputNumero,
         inputComplemento: req.body.inputComplemento,
-        inputBairro: req.body.inputBairro,
-        inputCEP: req.body.inputCEP,
-        inputCidade: req.body.inputCidade,
-        inputTelFixo: req.body.inputTelFixo,
-        inputRamal: req.body.inputRamal,
+        inputBairro: validaBairro(req.body.inputBairro),
+        inputCEP: validaCEP(req.body.inputCEP),
+        inputCidade: validaCidade(req.body.inputCidade),
+        inputTelFixo: validaTelFixo(req.body.inputTelFixo),
+        inputRamal: validaRamal(req.body.inputRamal),
         inputSetor: req.body.inputSetor
     };
     
@@ -28,8 +29,80 @@ app.post("/cadastrarTrabalho", urlEncodedParser, function(req, res){
     res.end(JSON.stringify(response));
 });
 
-// inserir as validações aqui.
-// inputEmpresa: validaEmpresa(req.body.inputEmpresa),
+function validaEmpresa(campo){
+    var campoValidar = campo.trim();
+    if(campoValidar == ""){
+        return "Erro: O campo 'Empresa' deve ser preenchido!'"
+    } else {
+        return  campo;
+    }
+};
+
+function validaLogradouro(campo){
+    var campoValidar = campo.trim();
+    if(campoValidar == ""){
+        return "Erro: O campo 'Logradouro' deve ser preenchido!'"
+    } else {
+        return  campo;
+    }    
+};
+
+function validaBairro(campo){
+    var campoValidar = campo.trim();
+    if(campoValidar == ""){
+        return "Erro: O campo 'Bairro' deve ser preenchido!'"
+    } else {
+        return  campo;
+    }  
+};
+
+function validaCEP(campo){
+    var campoValidar = campo.trim();
+    if(campoValidar == ""){
+        return "Erro: O campo 'CEP' deve ser preenchido!";
+    } else if(isNaN(campoValidar)){
+        return "Erro: O campo 'CEP' deve ter somente numeros!";
+    } else if (campoValidar.length != 8){
+        return "Erro: O campo 'CEP' deve ter 8 dígitos!";
+    } else {
+        return campo;
+    }
+};
+
+function validaCidade(campo){
+    var campoValidar = campo.trim();
+    if(campoValidar == ""){
+        return "Erro: O campo 'Cidade' deve ser preenchido!'"
+    } else {
+        return  campo;
+    }
+};
+
+function validaTelFixo(campo){
+    var campoValidar = campo.trim();
+    if(campoValidar == ""){
+        return "Erro: O campo 'Telefone fixo' deve ser preenchido!";
+    } else if(isNaN(campoValidar)){
+        return "Erro: O campo 'Telefone fixo' deve ter somente numeros!";
+    } else if (campoValidar.length != 10){
+        return "Erro: O campo 'Telefone fixo' deve ter 10 dígitos!";
+    } else {
+        return campo;
+    }    
+};
+
+function validaRamal(campo){
+    var campoValidar = campo.trim();
+    if(campoValidar == ""){
+        return "Erro: O campo 'Ramal' deve ser preenchido!";
+    } else if(isNaN(campoValidar)){
+        return "Erro: O campo 'Ramal' deve ter somente numeros!";
+    } else if (campoValidar.length != 10){
+        return "Erro: O campo 'Ramal' deve ter 10 dígitos!";
+    } else {
+        return campo;
+    }        
+};
 
 var server = app.listen(8081, function (){
     console.log("Server running at http://127.0.0.1:8081/");
--- static/Trabalho.js
+++ static/Trabalho.js
@@ -1,32 +1,88 @@
 function cadastraTrabalho(event){
     event.preventDefault();
     var request = new XMLHttpRequest();
+    var formValidado = true;
  
     request.onreadystatechange = function(){
         if (request.readyState == 4){
             if (request.status == 200){
                 var trabalho = JSON.parse(request.responseText);
                 console.log(trabalho);
-                var listaTrabalhos = document.getElementById("listaTrabalhos");
                 
-                //criar variável com if onde vai verificar se o que retornou do servidor esta igual ao enviado,
-                // caso contrário vai jogar o erro por appendChild no input em questão
+                if (trabalho.inputEmpresa != input_empresa.value){
+                    formValidado = false;
+                    document.getElementById("erroInputEmpresa").style.color = "#ff0000";
+                    document.getElementById("erroInputEmpresa").innerHTML = trabalho.inputEmpresa;
+                } else{
+                    document.getElementById("erroInputEmpresa").innerHTML = "";
+                }
 
-                listaTrabalhos.innerHTML =
-                    "<h1>" + "Dados cadastrados" + "</h1>" +
-                    "<ul>" +
-                        "<li>" + "Empresa: " + trabalho.inputEmpresa + "</li>" +
-                        "<li>" + "Logradouro: " + trabalho.inputLogradouro + "</li>" +
-                        "<li>" + "Numero: " + trabalho.inputNumero + "</li>" +
-                        "<li>" + "Complemento: " + trabalho.inputComplemento + "</li>" +
-                        "<li>" + "Bairro: " + trabalho.inputBairro + "</li>" +
-                        "<li>" + "CEP: " + trabalho.inputCEP + "</li>" +
-                        "<li>" + "Cidade: " + trabalho.inputCidade + "</li>" +
-                        "<li>" + "Telefone fixo: " + trabalho.inputTelFixo + "</li>" +
-                        "<li>" + "Ramal: " + trabalho.inputRamal + "</li>" +
-                        "<li>" + "Setor: " + trabalho.inputSetor + "</li>" +
-                    "</ul>";
-                listaTrabalhos.appendChild(itemtrabalho);                 
+                if (trabalho.inputLogradouro != input_logradouro.value){
+                    formValidado = false;
+                    document.getElementById("erroInputLogradouro").style.color = "#ff0000";
+                    document.getElementById("erroInputLogradouro").innerHTML = trabalho.inputLogradouro;
+                } else{
+                    document.getElementById("erroInputLogradouro").innerHTML = "";
+                }
+
+                if (trabalho.inputBairro != input_bairro.value){
+                    formValidado = false;
+                    document.getElementById("erroInputBairro").style.color = "#ff0000";
+                    document.getElementById("erroInputBairro").innerHTML = trabalho.inputBairro;
+                } else{
+                    document.getElementById("erroInputBairro").innerHTML = "";
+                }
+
+                if (trabalho.inputCEP != input_cep.value){
+                    formValidado = false;
+                    document.getElementById("erroInputCEP").style.color = "#ff0000";
+                    document.getElementById("erroInputCEP").innerHTML = trabalho.inputCEP;
+                } else{
+                    document.getElementById("erroInputCEP").innerHTML = "";
+                }
+
+                if (trabalho.inputCidade != input_cidade.value){
+                    formValidado = false;
+                    document.getElementById("erroInputCidade").style.color = "#ff0000";
+                    document.getElementById("erroInputCidade").innerHTML = trabalho.inputCidade;
+                } else{
+                    document.getElementById("erroInputCidade").innerHTML = "";
+                }
+
+                if (trabalho.inputTelFixo != input_telfixo.value){
+                    formValidado = false;
+                    document.getElementById("erroInputTelFixo").style.color = "#ff0000";
+                    document.getElementById("erroInputTelFixo").innerHTML = trabalho.inputTelFixo;
+                } else{
+                    document.getElementById("erroInputTelFixo").innerHTML = "";
+                }
+
+                if (trabalho.inputRamal != input_ramal.value){
+                    formValidado = false;
+                    document.getElementById("erroInputRamal").style.color = "#ff0000";
+                    document.getElementById("erroInputRamal").innerHTML = trabalho.inputRamal;
+                } else{
+                    document.getElementById("erroInputRamal").innerHTML = "";
+                }                
+
+                if (formValidado){
+                    var listaTrabalhos = document.getElementById("listaTrabalhos");
+                    listaTrabalhos.innerHTML =
+                        "<h1>" + "Dados cadastrados" + "</h1>" +
+                        "<ul>" +
+                            "<li>" + "Empresa: " + trabalho.inputEmpresa + "</li>" +
+                            "<li>" + "Logradouro: " + trabalho.inputLogradouro + "</li>" +
+                            "<li>" + "Numero: " + trabalho.inputNumero + "</li>" +
+                            "<li>" + "Complemento: " + trabalho.inputComplemento + "</li>" +
+                            "<li>" + "Bairro: " + trabalho.inputBairro + "</li>" +
+                            "<li>" + "CEP: " + trabalho.inputCEP + "</li>" +
+                            "<li>" + "Cidade: " + trabalho.inputCidade + "</li>" +
+                            "<li>" + "Telefone fixo: " + trabalho.inputTelFixo + "</li>" +
+                            "<li>" + "Ramal: " + trabalho.inputRamal + "</li>" +
+                            "<li>" + "Setor: " + trabalho.inputSetor + "</li>" +
+                        "</ul>";
+                    listaTrabalhos.appendChild(itemtrabalho);
+                }                 
             }   
         }
     };
